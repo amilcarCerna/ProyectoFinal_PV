0975e8e11b742529d0cd4112ce73440985dc5f32
Amilcar Cerna Rich <93951838+amilcarCerna@users.noreply.github.com>
2024-06-01 22:26:10 -0600

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.0")
+    }
+}
+
+
